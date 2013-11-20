f52e1c36cb7ec5059fbfe929506fb1d1476db8fb
Julian Geiger <jageiger@asu.edu>
2013-11-20 14:54:44 -0700

    Events Improved
    (Still awaiting invites to be expanded on.)

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,37 +1,46 @@
 class EventsController < ApplicationController
-	def new
-		@event = Event.new
-	end
-	
-	def create
-		@event = Event.new(params[:event].permit(:file))
-		@event.save
-	end
-		
-	def destroy
-		@event = Event.find(params[:id])
-		@event.destroy
-	end
-	
-	def edit
-		@event = Event.find(params[:id])
-	end
-	
-	def update
-		@event = Event.find(params[:id])
-		@event.update(params[:event].permit(:file))
-	end
-	
-	def show
-		@event = Event.find(params[:id])
-	end
-	
-	def index
-		@event = Event.all
-	end
-	
+        def new
+                @event = Event.new
+        end
+        
+        def create
+                @event = Event.new(params[:event].permit(:title, :description, :date, :time, :location))
+            
+                if @event.save
+                  redirect_to @event
+                else
+                  render 'new'
+                end
+        end
+        
+    
+        def destroy
+                @event = Event.find(params[:id])
+                @event.destroy
+        end
+        
+        def edit
+                @event = Event.find(params[:id])
+        end
+        
+        def update
+                @event = Event.find(params[:id])
+                if @event.update(params[:event].permit(:title, :description, :date, :time, :location))
+                   redirect_to @event
+                   else
+                     render 'edit'
+                   end
+        end
+        
+        
+        def show
+                @event = Event.find(params[:id])
+        end
+        
+        def index
+              @events = Event.all
+        end
+        
 
-	
+        
 end
-
-
